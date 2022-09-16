e61e7f4b6c0e39afc48098217c869cf39bf84b46
n-man13 <email4nikhil@gmail.com>
2022-09-16 09:44:39 -0400

    added userinterface class

---
+++ CRS-Group-B-JAVA-POS/src/com/lti/app/CRSUserInterface.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.lti.app;
+
+/**
+ * @author user101
+ *
+ */
+public class CRSUserInterface {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
