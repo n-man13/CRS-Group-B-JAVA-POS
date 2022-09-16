d599e43140f95c14c3ef6168edc3226afc558200
user105 <user105@Javafullstack37.alchemycloud.co.in>
2022-09-16 11:49:55 -0500

    commiting to fix pull error

--- CRS-Group-B-JAVA-POS/src/com/lti/app/TestApp.java
+++ CRS-Group-B-JAVA-POS/src/com/lti/app/TestApp.java
@@ -1,14 +1,1 @@
-package com.lti.app;
 
-import java.util.Scanner;
-
-public class TestApp {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		// Commit 1
-		
-		}
-	}
-
-}
