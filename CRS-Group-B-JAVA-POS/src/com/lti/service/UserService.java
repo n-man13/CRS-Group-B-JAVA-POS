71c6ec5149bac71b58f3069c24e0ede880b3cbb1
n-man13 <email4nikhil@gmail.com>
2022-09-16 15:01:44 -0400

    Added return type for verify

--- CRS-Group-B-JAVA-POS/src/com/lti/service/UserService.java
+++ CRS-Group-B-JAVA-POS/src/com/lti/service/UserService.java
@@ -2,9 +2,10 @@ package com.lti.service;
 
 public class UserService implements UserServiceInterface {
 
-	public void verifyCredetials(String username, String password) {
+	public boolean verifyCredetials(String username, String password) {
 		// verify credentials for all users
 		// need to hash password and store in a hashmap
+		return true;
 	}
 
 	public void logOut() {
--- CRS-Group-B-JAVA-POS/src/com/lti/service/UserServiceInterface.java
+++ CRS-Group-B-JAVA-POS/src/com/lti/service/UserServiceInterface.java
@@ -2,7 +2,7 @@ package com.lti.service;
 
 //user service interface
 public interface UserServiceInterface {
-	public void verifyCredetials(String username, String password);
+	public boolean verifyCredetials(String username, String password);
 
 	public void logOut();
 }
