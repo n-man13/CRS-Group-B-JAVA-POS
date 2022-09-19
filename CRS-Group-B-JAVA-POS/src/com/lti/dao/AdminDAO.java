4a10f3ca8ba7c459c2a12dde5838f1f696e08a9b
n-man13 <email4nikhil@gmail.com>
2022-09-19 15:56:22 -0400

    created AdminDAO

---
+++ CRS-Group-B-JAVA-POS/src/com/lti/dao/AdminDAO.java
@@ -0,0 +1,33 @@
+package com.lti.dao;
+
+import java.util.ArrayList;
+
+import com.lti.bean.Course;
+
+public class AdminDAO implements AdminDAOInterface {
+
+	@Override
+	public boolean createCourse(Course course) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean updateCourse(Course course) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Course deleteCourse(Course course) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<Course> listAllCourses() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
