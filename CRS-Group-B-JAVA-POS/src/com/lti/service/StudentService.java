6a91dab1cd88cfe93dd9d132d0f7c3a8d0cf2c8e
n-man13 <email4nikhil@gmail.com>
2022-09-19 12:21:12 -0400

    Added functionality to StudentService

--- CRS-Group-B-JAVA-POS/src/com/lti/service/StudentService.java
+++ CRS-Group-B-JAVA-POS/src/com/lti/service/StudentService.java
@@ -6,51 +6,62 @@ import com.lti.bean.Course;
 import com.lti.bean.Student;
 
 public class StudentService implements StudentServiceInterface {
-	
+
 	private Student student;
 	private static int newestID = 0;
-	
+
 	public StudentService() {
 		student = new Student(++newestID);
 	}
-	
+
 	public StudentService(int studentID) {
-		
+
 	}
+
 	public void applyToCourse(Student student, Course course) {
 		ArrayList<Course> courses = student.getRegisteredCourses();
 		System.out.println("Currently registered in the following: " + courses);
-		
+
 		courses.add(course);
 		System.out.println("Now registered in the following: " + courses);
 
 		student.setRegisteredCourses(courses);
-		
-		
-		
-		
-		
-		//adds course from applied list
+
+		// adds course from applied list
 
 	}
-	
-	public void dropCourse() {
-		//remove course from applied list
+
+	public Course dropCourse(Student student, int courseID) {
+		Course c = null;
+		ArrayList<Course> myCourses = student.getRegisteredCourses();
+		for (int i = 0; i < myCourses.size(); i++) {
+			if (myCourses.get(i).getCourseID() == courseID) {
+				myCourses.remove(i);
+				System.out.println("Course dropped");
+				return c;
+			}
+		}
+		System.err.println("Course not found"); // throw exception
+		return c;
+		// remove course from applied list
 	}
-	
-	public void viewAppliedCourses() {
-		//view list of applied courses
+
+	public void viewAppliedCourses() { // obsolete method
+		// view list of applied courses
 	}
-	
+
 	public void makePayment() {
-		//make payment
+		// make payment
 	}
-	
-	public void checkGrades() {
+
+	public void checkGrades(Student student) {
+		for (Course c : student.getRegisteredCourses()) {
+			c.getCourseID(); // replace with grade finder
+		}
 		// view grades
 	}
-	
+
 	public void listAllCourses() {
-		//view all courses 
+		// view all courses
 	}
 }
--- CRS-Group-B-JAVA-POS/src/com/lti/service/StudentServiceInterface.java
+++ CRS-Group-B-JAVA-POS/src/com/lti/service/StudentServiceInterface.java
@@ -5,13 +5,29 @@ import com.lti.bean.Student;
 
 //student service interface
 public interface StudentServiceInterface {
+	/**
+	 * Add student to specified course
+	 * @param student Student to add a course
+	 * @param course Course to add
+	 */
 	public void applyToCourse(Student student, Course course) ;
-	
-	public void dropCourse();
+	/**
+	 * Remove student from course
+	 * @param student Student to drop course
+	 * @param courseID Course to drop
+	 * @return Course if successful, else null
+	 */
+	public Course dropCourse(Student student, int courseID);
 	public void viewAppliedCourses();
-	
+	/**
+	 * Makes payment for courses
+	 */
 	public void makePayment();
-	public void checkGrades() ;
+	/**
+	 * Check grades for student
+	 * @param student Student to check grades for
+	 */
+	public void checkGrades(Student student) ;
 	
 	public void listAllCourses();
 
