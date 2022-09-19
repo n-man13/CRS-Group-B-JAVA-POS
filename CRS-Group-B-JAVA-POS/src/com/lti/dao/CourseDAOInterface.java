0af67ede5aeb202cc62005990d158ecebd26e73d
n-man13 <email4nikhil@gmail.com>
2022-09-19 16:22:03 -0400

    added CourseDAOInterface

---
+++ CRS-Group-B-JAVA-POS/src/com/lti/dao/CourseDAOInterface.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+package com.lti.dao;
+
+import java.util.ArrayList;
+
+import com.lti.bean.*;
+
+/**
+ * @author user101
+ *
+ */
+public interface CourseDAOInterface {
+	
+	/**
+	 * returns all courses in an ArrayList
+	 * @return all courses
+	 */
+	public ArrayList<Course> viewAllCourses();
+	
+	/**
+	 * 
+	 * @param course course to create
+	 * @return if course was created
+	 */
+	public boolean createCourse(Course course);
+	
+	/**
+	 * 
+	 * @param course the course to modify
+	 * @param prof the professor to teach
+	 * @return if course was updated
+	 */
+	public boolean addProfessor(Course course, Professor prof);
+	
+	
+	/**
+	 * 
+	 * @param course the course to delete
+	 * @return the course that was deleted, else null
+	 */
+	public Course deleteCourse(Course course);
+}
