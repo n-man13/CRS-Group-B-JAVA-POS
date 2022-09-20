91297af4b54f364f0cd3848a4c2524894b369cbb
n-man13 <email4nikhil@gmail.com>
2022-09-20 10:31:09 -0400

    added script to create tables in DB

---
+++ CRS-Group-B-JAVA-POS/src/com/lti/utils/tableCreation.sql
@@ -0,0 +1,68 @@
+create database if not exists CRSDatabase
+use CRSDatabase
+
+CREATE TABLE Roles(
+	roleID int NOT NULL UNIQUE,
+	name varchar(20) NOT NULL,
+	description varchar(200),
+	CONSTRAINT PK_RoleID PRIMARY KEY (roleID)
+);
+
+CREATE TABLE User(
+	userID int NOT NULL AUTO INCREMENT,
+	username varchar(24) NOT NULL UNIQUE,
+	password varchar(24) NOT NULL,
+	role int NOT NULL,
+	CONSTRAINT PK_UserID PRIMARY KEY (userID),
+	CONSTRAINT FK_Role FOREIGN KEY (role)
+	REFERENCES Roles(roleID)
+);
+
+CREATE TABLE Student(
+	studentID int NOT NULL UNIQUE,
+	name varchar(60) NOT NULL,
+	CONSTRAINT PK_Student PRIMARY KEY (studentID),
+	CONSTRAINT FK_StudentID FOREIGN KEY (studentID)
+	REFERENCES User(userID)
+);
+
+CREATE TABLE Professor(
+	professorID int NOT NULL UNIQUE,
+	name varchar(60) NOT NULL,
+	CONSTRAINT PK_Professor PRIMARY KEY (professorID),
+	CONSTRAINT FK_ProfesorID FOREIGN KEY (professorID)
+	REFERENCES User(userID)
+);
+
+CREATE TABLE Admin(
+	adminID int NOT NULL UNIQUE,
+	CONSTRAINT PK_Admin PRIMARY KEY (adminID),
+	CONSTRAINT FK_AdminID FOREIGN KEY (adminID)
+	REFERENCES User(userID)
+);
+
+CREATE TABLE Course(
+	courseID int NOT NULL AUTO INCREMENT,
+	name varchar(20),
+	department varchar(20),
+	description varchar(60),
+	professorID int,
+	prereqID int,
+	CONSTRAINT PK_Course PRIMARY KEY (courseID),
+	CONSTRAINT FK_ProfessorID FOREIGN KEY (professorID)
+	REFERENCES Professor(professorID),
+	CONSTRAINT FK_PrereqID FOREIGN KEY (prereqID)
+	REFERENCES Course(courseID)
+);
+
+CREATE TABLE SemesterRegistration(
+	studentID int NOT NULL,
+	courseID int NOT NULL,
+	feePaid bool DEFAULT 0,
+	grade double,
+	CONSTRAINT PK_StudentCourse PRIMARY KEY (studentID, courseID),
+	CONSTRAINT FK_StudentID FOREIGN KEY (studentID)
+	REFERENCES Student(studentID),
+	CONSTRAINT FK_CourseID FOREIGN KEY (courseID)
+	REFERENCES Course(courseID)
+);
