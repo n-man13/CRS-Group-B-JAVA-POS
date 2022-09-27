f8093437ccb6b503f00480865dcdb5b78ba8c453
n-man13 <email4nikhil@gmail.com>
2022-09-27 11:03:03 -0400

    added spring rest project

---
+++ CRS-Group-B-Spring-REST/src/main/java/com/lti/application/CrsGroupBSpringRestApplication.java
@@ -0,0 +1,13 @@
+package com.lti.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrsGroupBSpringRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrsGroupBSpringRestApplication.class, args);
+	}
+
+}
