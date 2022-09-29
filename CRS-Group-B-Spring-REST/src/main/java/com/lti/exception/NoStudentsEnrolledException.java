3a19fa77040f6a4364fb5527383c1f61f7bcd8c4
n-man13 <email4nikhil@gmail.com>
2022-09-29 20:41:00 +0530

    added responsestatus to all exceptions

--- CRS-Group-B-Spring-REST/src/main/java/com/lti/exception/NoStudentsEnrolledException.java
+++ CRS-Group-B-Spring-REST/src/main/java/com/lti/exception/NoStudentsEnrolledException.java
@@ -1,5 +1,9 @@
 package com.lti.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_ACCEPTABLE)
 public class NoStudentsEnrolledException extends Exception {
 
 	private int courseID;
