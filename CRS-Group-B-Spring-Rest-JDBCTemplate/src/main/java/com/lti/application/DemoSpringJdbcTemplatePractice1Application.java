4085653ecce53391bbd5615832d4aa6b1c383d8a
Luca Mastrofini <lucam96@yahoo.it>
2022-09-30 12:53:35 -0400

    new project

---
+++ CRS-Group-B-Spring-Rest-JDBCTemplate/src/main/java/com/lti/application/DemoSpringJdbcTemplatePractice1Application.java
@@ -0,0 +1,13 @@
+package com.lti.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringJdbcTemplatePractice1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringJdbcTemplatePractice1Application.class, args);
+	}
+
+}
---
+++ CRS-Group-B-Spring-Rest-JDBCTemplate/src/test/java/com/lti/application/DemoSpringJdbcTemplatePractice1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lti.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringJdbcTemplatePractice1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
