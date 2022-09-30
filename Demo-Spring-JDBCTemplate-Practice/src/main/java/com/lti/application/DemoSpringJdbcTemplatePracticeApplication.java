dc1e1d8290e7bc025244b733439257da945f3bb8
Luca Mastrofini <lucam96@yahoo.it>
2022-09-30 12:47:00 -0400

    practice demo jdbc t

---
+++ Demo-Spring-JDBCTemplate-Practice/src/main/java/com/lti/application/DemoSpringJdbcTemplatePracticeApplication.java
@@ -0,0 +1,21 @@
+package com.lti.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan({"com.lti.*"})
+@EnableWebMvc
+public class DemoSpringJdbcTemplatePracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringJdbcTemplatePracticeApplication.class, args);
+	}
+
+}
