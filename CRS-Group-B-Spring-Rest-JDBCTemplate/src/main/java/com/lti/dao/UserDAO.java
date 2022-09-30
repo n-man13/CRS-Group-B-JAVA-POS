caa17852c07e6686e771f8cdcc1de22648a9abe3
n-man13 <email4nikhil@gmail.com>
2022-10-01 00:23:52 +0530

    userDAO written

--- CRS-Group-B-Spring-Rest-JDBCTemplate/src/main/java/com/lti/dao/UserDAO.java
+++ CRS-Group-B-Spring-Rest-JDBCTemplate/src/main/java/com/lti/dao/UserDAO.java
@@ -3,7 +3,12 @@
  */
 package com.lti.dao;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import com.lti.configuration.JDBCConfiguration;
 import com.lti.dto.User;
+import com.lti.mapper.UserMapper;
 
 /**
  * @author user101
@@ -11,22 +16,32 @@ import com.lti.dto.User;
  */
 public class UserDAO implements UserDAOInterface {
 
+	@Autowired
+	private JDBCConfiguration jdbcTemplateObject;
+	// Queries for User table
+	public static final String USER_INSERT = "INSERT INTO User(username, password, role) VALUES(%s,%s,%o)";
+	public static final String USER_SELECT = "SELECT userID, username , password, role FROM User WHERE username = ?";
+	public static final String USER_UPDATE_PASSWORD = "UPDATE User SET password=%s WHERE username=%s";
+
 	@Override
 	public int createUser(String username, String password, int role) {
-		// TODO Auto-generated method stub
-		return 0;
+		jdbcTemplateObject.jdbcTemplate().execute(String.format(USER_INSERT, username, password, role));
+		User user = findUser(username);
+		return user.getUserID();
 	}
 
 	@Override
 	public User findUser(String username) {
-		// TODO Auto-generated method stub
-		return null;
+		String sql = "SELECT * FROM User WHERE username = ?";
+		User user = jdbcTemplateObject.jdbcTemplate().queryForObject(USER_SELECT, new Object[] { username },
+				new UserMapper());
+		return user;
 	}
 
 	@Override
 	public boolean updatePassword(String username, String password) {
-		// TODO Auto-generated method stub
-		return false;
+		jdbcTemplateObject.jdbcTemplate().execute(String.format(USER_UPDATE_PASSWORD, password, username));
+		return true;
 	}
 
 }
