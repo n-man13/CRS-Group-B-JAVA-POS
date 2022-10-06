8ba5a8bf0afc9459384d5fcc33f3e5724c7121b9
Luca Mastrofini <lucam96@yahoo.it>
2022-10-06 19:42:36 +0530

    Merge branch 'main' of https://github.com/n-man13/CRS-Group-B-JAVA-POS.git into main

---
+++ CRS-Group-B-Professor-Producer/src/main/java/com/lti/application/CrsGroupBProfessorProducerApplication.java
@@ -0,0 +1,13 @@
+package com.lti.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrsGroupBProfessorProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrsGroupBProfessorProducerApplication.class, args);
+	}
+
+}
