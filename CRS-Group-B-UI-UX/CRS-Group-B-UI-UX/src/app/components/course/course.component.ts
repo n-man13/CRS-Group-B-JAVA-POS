8c8b528fab4c6cfd0073436d40d69288740ba9e4
Luca Mastrofini <lucam96@yahoo.it>
2022-10-14 14:12:22 -0400

    component course

--- CRS-Group-B-UI-UX/CRS-Group-B-UI-UX/src/app/components/course/course.component.ts
+++ CRS-Group-B-UI-UX/CRS-Group-B-UI-UX/src/app/components/course/course.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Course } from 'src/app/model/course';
 
 @Component({
   selector: 'app-course',
@@ -10,6 +11,18 @@ export class CourseComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  createCourse() {
+
+  }
+
+  updateCourse(course:Course) {
+
+  }
+
+  deleteCourse(course:Course) {
+    
   }
 
 }
