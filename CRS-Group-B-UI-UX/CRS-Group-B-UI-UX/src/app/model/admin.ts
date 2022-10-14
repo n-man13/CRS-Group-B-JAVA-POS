4c2bcad04ed7e93eaa5cf288abb0443611c1af51
n-man13 <email4nikhil@gmail.com>
2022-10-14 13:44:49 -0400

    added admin model

--- CRS-Group-B-UI-UX/CRS-Group-B-UI-UX/src/app/model/admin.ts
+++ CRS-Group-B-UI-UX/CRS-Group-B-UI-UX/src/app/model/admin.ts
@@ -1,2 +1,10 @@
-export class Admin {
+import { User } from "./user";
+
+export class Admin extends User {
+    public adminID:number;
+
+    constructor(id:number, username:string, password:string){
+        super(id, username, password, 1);
+        this.adminID = id;
+    }
 }
