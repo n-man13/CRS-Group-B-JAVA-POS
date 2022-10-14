1ad32087b738bb1b61e6fde72d868e13632e326d
n-man13 <email4nikhil@gmail.com>
2022-10-14 14:09:50 -0400

    finished course

--- CRS-Group-B-UI-UX/CRS-Group-B-UI-UX/src/app/model/course.ts
+++ CRS-Group-B-UI-UX/CRS-Group-B-UI-UX/src/app/model/course.ts
@@ -1,2 +1,19 @@
+import { Professor } from "./professor";
+
 export class Course {
+    public courseID: number;
+    public name: string;
+    public description: string;
+    public department: string;
+    public professor: Professor;
+    public prereqCourse: Course;
+
+    constructor(courseID: number, name: string, description: string, department: string, prereqCourse: Course, professor: Professor) {
+        this.courseID = courseID;
+        this.name = name;
+        this.description = description;
+        this.department = department;
+        this.professor = professor;
+        this.prereqCourse = prereqCourse;
+    }
 }
