03e18f9b2d19cc5f0996a19c4e656bd8f7f7d350
n-man13 <email4nikhil@gmail.com>
2022-10-14 15:06:34 -0400

    continued work on professor

--- CRS-Group-B-UI-UX/CRS-Group-B-UI-UX/src/app/components/professor/professor.component.ts
+++ CRS-Group-B-UI-UX/CRS-Group-B-UI-UX/src/app/components/professor/professor.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Professor } from 'src/app/model/professor';
 
 @Component({
   selector: 'app-professor',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./professor.component.css']
 })
 export class ProfessorComponent implements OnInit {
+  professors: Array<Professor> = new Array();
+  id: number = 1;
+  model: Professor = new Professor(0, "", "", "");
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  createProfessor() {
+    this.professors.push(new Professor(this.id, this.model.name, this.model.username, this.model.password));
+    this.id++;
+    console.log("new professor added");
+  }
 }
