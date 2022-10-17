a90cc760e94ff02bfc49827459f9f25b8af2f3b8
Luca Mastrofini <lucam96@yahoo.it>
2022-10-17 19:41:14 +0530

    student reg

--- CRS-Group-B-UI-UX/CRS-Group-B-UI-UX/src/app/model/student.ts
+++ CRS-Group-B-UI-UX/CRS-Group-B-UI-UX/src/app/model/student.ts
@@ -3,10 +3,12 @@ import { User } from "./user";
 export class Student extends User{
     public studentID: number;
     public name: string;
+    public isRegistered:boolean;
 
-    constructor(id: number, name: string, username:string, password:string) {
+    constructor(id: number, name: string, username:string, password:string, isRegistered:boolean) {
         super(id, username, password, 3);
         this.studentID = id;
         this.name = name;
+        this.isRegistered = isRegistered;
     }
 }
