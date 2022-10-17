5fe9964674ece4e4cd50d254491b2f74f39cd9a5
n-man13 <email4nikhil@gmail.com>
2022-10-17 17:03:58 -0400

    list-unregistered finished

--- CRS-Group-B-UI-UX/CRS-Group-B-UI-UX/src/app/components/list-unregistered-student/list-unregistered-student.component.ts
+++ CRS-Group-B-UI-UX/CRS-Group-B-UI-UX/src/app/components/list-unregistered-student/list-unregistered-student.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Student } from 'src/app/model/student';
 
 @Component({
   selector: 'app-list-unregistered-student',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListUnregisteredStudentComponent implements OnInit {
 
-  constructor() { }
+  studentArray:Student[] = new Array();
+
+  constructor() { 
+    let s1:Student = new Student(1, "Luca", "Lucam", "1234", false);
+    let s2:Student = new Student(2, "Seb", "Seba", "1234", false);
+    let s3:Student = new Student(3, "Nikhil", "Nik", "1234", false);
+    this.studentArray.push(s1,s2,s3);
+    console.log(JSON.stringify(this.studentArray));
+  }
 
   ngOnInit(): void {
   }
 
+  studentRegistration(student:Student) {
+    console.log(JSON.stringify(student));
+    let index = this.studentArray.indexOf(student);
+    console.log(index);
+    console.log(JSON.stringify(this.studentArray[index]));
+    if (index != -1) {
+        this.studentArray[index].isRegistered = true;
+    }
+  }
+  
 }
