32b316b98d1823916577ebe6921712d4643c713d
n-man13 <email4nikhil@gmail.com>
2022-10-21 10:42:28 -0400

    more separation between admin items

--- CRS-Group-B-UI-UX/CRS-Group-B-UI-UX/src/app/model/registered-course.ts
+++ CRS-Group-B-UI-UX/CRS-Group-B-UI-UX/src/app/model/registered-course.ts
@@ -1,2 +1,13 @@
 export class RegisteredCourse {
+    public courseID: number;
+    public studentID: number;
+    public feePaid: boolean;
+    public grade: number;
+
+    constructor(courseID: number, studentID: number){
+        this.courseID = courseID;
+        this.studentID = studentID;
+        this.feePaid = false;
+        this.grade = -1;
+    }
 }
---
+++ CRS-Group-B-UI-UX/CRS-Group-B-UI-UX/src/app/services/admin/course.service.ts
@@ -0,0 +1,54 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Course } from '../../model/course';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CourseService {
+
+  url: string = "http://localhost:8091/";
+
+  headers = new HttpHeaders().set('Content-Type', 'application/json').set('Access-Control-Allow-Origin', "*");
+
+  constructor(private httpClient: HttpClient) { }
+
+  model:Course = new Course(0,"","","",0,0);
+
+
+  getCourses(): Observable<any> {
+
+    return this.httpClient.get(this.url + "viewAllCourses", { headers: this.headers });
+
+  }
+
+  createCourse(course: Course): Observable<any> {
+
+    const body = JSON.stringify(course);
+    return this.httpClient.post(this.url + "createCourse", body, { headers: this.headers });
+
+  }
+
+  updateCourse(course: Course): Observable<any> {
+
+    const body = JSON.stringify(course);
+    return this.httpClient.put(this.url + "updateCourse/" + course.courseID, body, { headers: this.headers });
+
+  }
+
+  deleteCourse(course: Course): Observable<any> {
+
+    const body = JSON.stringify(course);
+    let httpOptions = {};
+    return this.httpClient.delete(this.url + "deleteCourse/" + course.courseID, httpOptions);
+
+  }
+
+  saveInfo(course: Course) {
+    this.model = course;
+  }
+
+}
+
+
