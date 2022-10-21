7401d60b85b0a3cce288ad946982dc93b998a7ba
n-man13 <email4nikhil@gmail.com>
2022-10-21 11:13:21 -0400

    refactoring old services better

--- CRS-Group-B-UI-UX/CRS-Group-B-UI-UX/src/app/services/admin/course.service.ts
+++ CRS-Group-B-UI-UX/CRS-Group-B-UI-UX/src/app/services/admin/course.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Course } from '../../model/course';
+import { Course } from 'src/app/model/course';
 
 @Injectable({
   providedIn: 'root'
--- CRS-Group-B-UI-UX/CRS-Group-B-UI-UX/src/app/services/admin/student.service.ts
+++ CRS-Group-B-UI-UX/CRS-Group-B-UI-UX/src/app/services/admin/student.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Student } from '../../model/student';
+import { Student } from 'src/app/model/student';
 
 @Injectable({
   providedIn: 'root'
