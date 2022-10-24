c3289fef97240fa3b810d1d4cf0b198f8ca4de1e
n-man13 <email4nikhil@gmail.com>
2022-10-24 10:52:50 -0400

    various changes

--- CRS-Group-B-UI-UX/CRS-Group-B-UI-UX/src/app/components/Student/check-grades/check-grades.component.ts
+++ CRS-Group-B-UI-UX/CRS-Group-B-UI-UX/src/app/components/Student/check-grades/check-grades.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Course } from 'src/app/model/course';
+import { Grade } from 'src/app/model/grade';
+import { Student } from 'src/app/model/student';
+import { RegisteredCourseService } from 'src/app/services/student/registered-course.service';
 
 @Component({
   selector: 'app-check-grades',
@@ -7,11 +11,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CheckGradesComponent implements OnInit {
 
-  constructor() { }
+  student: Student = new Student(1, "Luca", "Lucam", "1234", true);
+  grades: Map<Course, Grade> = new Map();
+  courses: Course[] = new Array();
+
+  constructor(private registeredCourseService: RegisteredCourseService) {}
 
   ngOnInit(): void {
+    this.getCourses();
+    this.getGrades();
+    
   }
 
-  
+  getGrades(){
+    console.log("running grades")
+    this.registeredCourseService.checkGrades(this.student).subscribe(data => {
+      console.log(data);
+      this.grades = data;
+      this.getCourses();
+    });
+    
+  }
+
+  getCourses(){
+    console.log("running courses");
+    this.grades.forEach(function(value,key){
+      console.log(key.name);
+    })
+    for (let course of this.grades.keys()){
+      console.log(course.name);
+      this.courses.push(course);
+    } 
+  }
 
 }
---
+++ CRS-Group-B-UI-UX/CRS-Group-B-UI-UX/src/app/model/grade.ts
@@ -0,0 +1,9 @@
+export class Grade {
+    public grade:number;
+    public gradeLetter:String;
+
+    constructor(grade: number, gradeLetter: String){
+        this.grade = grade;
+        this.gradeLetter = gradeLetter;
+    }
+}
