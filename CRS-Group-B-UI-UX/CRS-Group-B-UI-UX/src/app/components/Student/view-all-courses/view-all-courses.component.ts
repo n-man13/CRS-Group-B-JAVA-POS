9b0105f9be5c875eda80b5bc5eed0dda199486f7
Luca Mastrofini <lucam96@yahoo.it>
2022-10-24 20:14:31 +0530

    changes

--- CRS-Group-B-UI-UX/CRS-Group-B-UI-UX/src/app/components/Student/view-all-courses/view-all-courses.component.ts
+++ CRS-Group-B-UI-UX/CRS-Group-B-UI-UX/src/app/components/Student/view-all-courses/view-all-courses.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Course } from 'src/app/model/course';
 import { Student } from 'src/app/model/student';
 import { CourseService } from 'src/app/services/student/course.service';
@@ -11,25 +12,25 @@ import { RegisteredCourseService } from 'src/app/services/student/registered-cou
 })
 export class ViewAllCoursesComponent implements OnInit {
 
-  student:Student = new Student(1, "Luca", "Lucam", "1234", true);
+  student: Student = new Student(1, "Luca", "Lucam", "1234", true);
 
   allCourses: Course[] | undefined;
 
-  myCourses: Course [] = [];
+  myCourses: Course[] = [];
 
-  constructor(private courseService: CourseService, private registeredCourseService: RegisteredCourseService) { }
+  constructor(private courseService: CourseService, private registeredCourseService: RegisteredCourseService, public router: Router) { }
 
   ngOnInit(): void {
 
     this.getCourses();
-    this.getAppliedCourse();    
+    this.getAppliedCourse();
 
   }
 
   getCourses() {
 
     this.courseService.getCourses().subscribe(data => {
-      console.log("GetCourses DATA"+data);
+      console.log("GetCourses DATA" + data);
       this.allCourses = data;
       console.log("all courses array" + this.allCourses);
 
@@ -40,21 +41,39 @@ export class ViewAllCoursesComponent implements OnInit {
   getAppliedCourse() {
 
     this.courseService.getAppliedCourses(this.student).subscribe(data => {
-      console.log("applied course Data "+data);
+      console.log("applied course Data " + data);
       this.myCourses = data;
-      
+
     })
     console.log("my courses array" + this.myCourses);
 
   }
 
-  applyToCourse(course:Course) {
+  applyToCourse(course: Course) {
 
     this.registeredCourseService.addCourse(this.student, course).subscribe(data => {
       console.log(data);
-      this.getCourses();
+      location.reload();
+      // this.router.navigate(['viewallcourses'])
+      // this.getCourses();
     })
-
+    
   }
 
+  isApplied(course: Course) {
+    let output:boolean = false;
+    this.myCourses.forEach(myCourse => {
+      if (myCourse.courseID == course.courseID) {
+        output = true;
+      }
+
+    })
+    return output;
+  }
+
+  goToAppliedCourses() {
+
+    this.router.navigate(['viewappliedcourses'])
+
+  }
 }
--- CRS-Group-B-UI-UX/CRS-Group-B-UI-UX/src/app/components/Student/view-applied-courses/view-applied-courses.component.ts
+++ CRS-Group-B-UI-UX/CRS-Group-B-UI-UX/src/app/components/Student/view-applied-courses/view-applied-courses.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Course } from 'src/app/model/course';
+import { Student } from 'src/app/model/student';
+import { CourseService } from 'src/app/services/student/course.service';
+import { RegisteredCourseService } from 'src/app/services/student/registered-course.service';
 
 @Component({
   selector: 'app-view-applied-courses',
@@ -7,16 +11,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewAppliedCoursesComponent implements OnInit {
 
-  constructor() { }
+  student:Student = new Student(1, "Luca", "Lucam", "1234", true);
+
+  allCourses: Course[] | undefined;
+
+  myCourses: Course [] = [];
+
+  constructor(private courseService: CourseService, private registeredCourseService: RegisteredCourseService) { }
 
   ngOnInit(): void {
+
+    
+    this.getAppliedCourse();    
+
   }
+
+  getAppliedCourse() {
+
+    this.courseService.getAppliedCourses(this.student).subscribe(data => {
+      console.log("applied course Data "+data);
+      this.myCourses = data;
+      
+    })
+    console.log("my courses array" + this.myCourses);
+
+  }
+
+
 payCourse() {
 
 }
 
 dropCourse() {
-  
+
 }
 
 }
