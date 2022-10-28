d6fca92a71e5a6f0e6aabd6740dd54b7137ec351
Luca Mastrofini <lucam96@yahoo.it>
2022-10-28 20:21:59 +0530

    ok

---
+++ CRS-Group-B-Express-Login-API/ExpressAPI/expressAPI.js
@@ -0,0 +1,42 @@
+const express = require('express');
+var Dao = require('../ExpressDAO/loginDAO');
+var dao = new Dao();
+const app = express();
+// respond with "hello world" when a GET request is made to the homepage
+app.post('/login', express.json(), (req, res) => {
+    dao.login(function(err, result){
+        res.send(result);
+    });
+});
+
+const URL_PUT = '/updateCustomer';  // final in java // final C#
+app.put(URL_PUT, express.json(), function (req, res) {
+    dao.updateCustomer(req.body, function(err, result){
+        res.send(result);
+    });
+})
+
+const URL_POST = '/createCustomer';  // final in java // final C#
+app.post(URL_POST,express.json(), function (req, res) {
+    dao.createCustomer(req.body, function(err, result){
+        res.send(result);
+    });    
+})
+// delete implementation here 
+app.delete('/deleteCustomer',express.json(), function (req, res) {
+    dao.deleteCustomer(req.body, function(err, result){
+        res.send(result);
+    });
+})
+
+// craete server here  to expose the Express code
+var server = app.listen(3001, function () {
+    var host = server.address().address
+    var port = server.address().port
+    console.log("Example app listening at http://%s:%s", host, port)
+})
+
+
+
+
+
--- CRS-Group-B-Express-Login-API/ExpressDAO/loginDAO.js
+++ CRS-Group-B-Express-Login-API/ExpressDAO/loginDAO.js
@@ -6,3 +6,4 @@ var con = mysql.createConnection({
     password: "root",
     database: "crsdatabase"
 });
+
