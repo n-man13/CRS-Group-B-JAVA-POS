0e06bf522de760c3b3f9b50f658ff88d8cf75fac
n-man13 <email4nikhil@gmail.com>
2022-10-28 10:49:28 -0400

    added queries

--- CRS-Group-B-Express-Login-API/ExpressDAO/loginDAO.js
+++ CRS-Group-B-Express-Login-API/ExpressDAO/loginDAO.js
@@ -6,3 +6,8 @@ var con = mysql.createConnection({
     password: "root",
     database: "crsdatabase"
 });
+
+var USER_PASSWORD = "SELECT userID, username , password, role FROM User WHERE userID = ?";
+var STUDENT_JOIN = "SELECT User.userID, User.username, User.role, Student.studentID, Student.name, Student.registrationApproved FROM Student INNER JOIN User ON User.userID=Student.studentID;";
+var PROFESSOR_JOIN = "SELECT User.userID, User.username, User.role, Professor.professorID, Professor.name FROM Professor INNER JOIN User ON User.userID=Professor.professorID;";
+var ADMIN_JOIN = "SELECT User.userID, User.username, User.role, Admin.adminID FROM Admin INNER JOIN User ON User.userID=Admin.adminID;";
