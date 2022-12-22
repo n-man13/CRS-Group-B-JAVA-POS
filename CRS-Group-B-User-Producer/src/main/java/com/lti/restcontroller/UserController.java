b95844bba03995641e4c84756abdc4ec053381af
Nikhil Shah <email4nikhil@gmail.com>
2022-12-22 10:26:13 -0500

    started with second rest api

--- CRS-Group-B-User-Producer/src/main/java/com/lti/restcontroller/UserController.java
+++ CRS-Group-B-User-Producer/src/main/java/com/lti/restcontroller/UserController.java
@@ -8,13 +8,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lti.dto.Admin;
+import com.lti.dto.ClientDetail;
 import com.lti.dto.Professor;
 import com.lti.dto.Student;
 import com.lti.dto.User;
@@ -42,7 +42,7 @@ public class UserController {
 	 * @throws StudentNotFoundException if student is not found
 	 */
 	@RequestMapping(consumes = MediaType.APPLICATION_JSON, method = RequestMethod.POST, value="/login")
-	public ResponseEntity login(@RequestBody User user) throws StudentNotFoundException {
+	public ResponseEntity<?> login(@RequestBody User user) throws StudentNotFoundException {
 		logger.info("login in userController");
 		if (userService.verifyCredetials(user.getUsername(), user.getPassword(), user.getRole())) {
 			switch(user.getRole()) {
@@ -52,7 +52,7 @@ public class UserController {
 				result.setRole(user.getRole());
 				result.setUserID(userService.getUserID(user.getUsername()));
 				result.setAdminID(result.getUserID());
-				return new ResponseEntity(result, HttpStatus.OK);
+				return new ResponseEntity<>(result, HttpStatus.OK);
 			case 2:
 				Professor resultP = new Professor();
 				resultP.setUsername(user.getUsername());
@@ -60,29 +60,29 @@ public class UserController {
 				resultP.setUserID(userService.getUserID(user.getUsername()));
 				resultP.setProfessorID(resultP.getUserID());
 				resultP.setName(userService.getProfessor(resultP.getUserID()).getName());
-				return new ResponseEntity(resultP, HttpStatus.OK);
+				return new ResponseEntity<>(resultP, HttpStatus.OK);
 			case 3:
 				Student resultS = studentService.getStudentByUsername(user.getUsername());
 				resultS.setUsername(user.getUsername());
 				resultS.setRole(user.getRole());
 				resultS.setUserID(userService.getUserID(user.getUsername()));
-				return new ResponseEntity(resultS, HttpStatus.OK);
+				return new ResponseEntity<>(resultS, HttpStatus.OK);
 			}
 			User newUser = new User();
 			newUser.setUsername(user.getUsername());
 			newUser.setRole(user.getRole());
 			newUser.setUserID(userService.getUserID(user.getUsername()));
-			return new ResponseEntity(newUser, HttpStatus.OK);
+			return new ResponseEntity<>(newUser, HttpStatus.OK);
 		}
 		
 		if (user == null)
-			return new ResponseEntity("User information not provided", HttpStatus.NOT_FOUND);
+			return new ResponseEntity<>("User information not provided", HttpStatus.NOT_FOUND);
 //		try {
 //			userService.verifyCredetials(user.getUsername(), user.getPassword(), user.getRole());
 //		} catch (StudentNotFoundException e) {
 //			return new ResponseEntity(e.getMessage() + e.getStudentID(), HttpStatus.NOT_FOUND);
 //		}
-		return new ResponseEntity("Login failed",HttpStatus.UNAUTHORIZED);
+		return new ResponseEntity<>("Login failed",HttpStatus.UNAUTHORIZED);
 	}
 	
 	/**
@@ -94,7 +94,7 @@ public class UserController {
 	 * @throws StudentNotFoundException 
 	 */
 	@RequestMapping(value = "/studentRegistration", method = RequestMethod.POST)
-	public ResponseEntity studentRegistration(@RequestBody Student student) throws UsernameUsedException, StudentNotFoundException {
+	public ResponseEntity<?> studentRegistration(@RequestBody Student student) throws UsernameUsedException, StudentNotFoundException {
 		logger.info("studentRegistration in userController");
 		studentService.createStudent(student);
 		Student newStudent = studentService.getStudentByUsername(student.getUsername());
@@ -102,13 +102,13 @@ public class UserController {
 		newStudent.setRole(3);
 		newStudent.setUserID(userService.getUserID(student.getUsername()));
 		if (student == null)
-			return new ResponseEntity("New Student information not provided", HttpStatus.NOT_FOUND);
+			return new ResponseEntity<>("New Student information not provided", HttpStatus.NOT_FOUND);
 //		try {
 //			studentService.createStudent(student);
 //		} catch (UsernameUsedException e) {
 //			return new ResponseEntity(e.getMessage() + e.getUsername(), HttpStatus.I_AM_A_TEAPOT);
 //		}
-		return new ResponseEntity(newStudent, HttpStatus.CREATED);
+		return new ResponseEntity<>(newStudent, HttpStatus.CREATED);
 	}
 	
 	/**
@@ -118,11 +118,16 @@ public class UserController {
 	 * @return ResponseEntity the ResponseEntity if the login is successful or not
 	 */
 	@RequestMapping(value = "/updatePassword", method = RequestMethod.POST)
-	public ResponseEntity updatePassword(@RequestBody User user) {
+	public ResponseEntity<?> updatePassword(@RequestBody User user) {
 		logger.info("updatePassword in userController");
 		if (user == null)
-			return new ResponseEntity("User information not provided", HttpStatus.NOT_FOUND);
+			return new ResponseEntity<>("User information not provided", HttpStatus.NOT_FOUND);
 		userService.updatePassword(user.getUsername(), user.getPassword());
-		return new ResponseEntity(HttpStatus.OK);
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
+
+	@RequestMapping(value = "/logClientData", consumes = MediaType.APPLICATION_JSON, method = RequestMethod.POST)
+	public ResponseEntity<?> logClientData(@RequestBody ClientDetail clientData){
+		return new ResponseEntity<>("Not implemented", HttpStatus.NOT_IMPLEMENTED);
 	}
 }
